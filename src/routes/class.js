28fe2c13acbe8d845ef64a9d058e774590941a4b
alvita25 <alvitamd25@gmail.com>
2024-12-05 22:31:57 +0530

    added delete class timings

--- src/routes/class.js
+++ src/routes/class.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { addClass } = require('../controllers/class_controllers/addClass.js');
+const { deleteClass } = require('../controllers/class_controllers/deleteClass.js');
 
 router.put('/', addClass);
+router.delete('/:classId', deleteClass);
 
 module.exports = router;
