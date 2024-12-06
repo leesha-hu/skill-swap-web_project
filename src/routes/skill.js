f26f77fbae0a55b00ed22b01f53493cfdc6a88fe
alvita25 <alvitamd25@gmail.com>
2024-12-06 19:46:36 +0530

    added retrieving user skills

--- src/routes/skill.js
+++ src/routes/skill.js
@@ -6,8 +6,10 @@ const { getSkills } = require('../controllers/skill_controllers/getAllSkills.js'
 const { updateSkill } = require('../controllers/skill_controllers/updateSkill.js');
 const { deleteSkill } = require('../controllers/skill_controllers/deleteSkill.js');
 const { getCatSkills } = require('../controllers/skill_controllers/getCategorySkills.js');
+const { getUserSkills } = require('../controllers/skill_controllers/getUserSkills.js');
 
 router.get('/:categoryId', getCatSkills);
+router.get('/user/:userId', getUserSkills);
 router.get('/', getSkills);
 router.post('/', addSkill);
 router.put('/', updateSkill);
