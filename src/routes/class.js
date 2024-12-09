e5c47c13d3ff27a467411bb93ce25886bf285253
alvita25 <alvitamd25@gmail.com>
2024-12-09 19:45:36 +0530

    added fuction to get classes that the user is teaching

--- src/routes/class.js
+++ src/routes/class.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const { addClass } = require('../controllers/class_controllers/addClass.js');
 const { deleteClass } = require('../controllers/class_controllers/deleteClass.js');
+const { getUserClasses } = require('../controllers/class_controllers/getUserClasses.js');
 
 router.put('/', addClass);
 router.delete('/:classId', deleteClass);
+router.get('/:userId', getUserClasses)
 
 module.exports = router;
