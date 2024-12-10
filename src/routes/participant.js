cb78a0c8a5f8dc385af55ab87865463e50ab398b
alvita25 <alvitamd25@gmail.com>
2024-12-10 20:32:26 +0530

    added getting user participation

--- src/routes/participant.js
+++ src/routes/participant.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { enrollParticipant } = require('../controllers/participant_controllers/enroll_participant.js');
+const { getParticipatingClasses } = require('../controllers/participant_controllers/getUserParticipation.js')
 
 router.post('/:userId/:classId', enrollParticipant);
+router.get('/:userId', getParticipatingClasses);
 
 module.exports = router;
