07fcf530c3c6df98feb5253b5c3323a52802f2ad
alvita25 <alvitamd25@gmail.com>
2025-01-04 11:25:21 +0530

    session name is updated when changing the name

--- src/controllers/user_controllers/changeName.js
+++ src/controllers/user_controllers/changeName.js
@@ -9,7 +9,7 @@ const changeName = async (req, res) => {
         console.log('not logged in!');
         return res.json({ message: 'not logged in!' });
     }
-    
+
     // check if the name is at least 3 characters long 
     if (!name.match(/(?=.*[a-zA-Z0-9]{3,}).*$/g)) {
         return res.status(400).json({ error: 'Name should be at least 3 chars long' });
@@ -18,6 +18,7 @@ const changeName = async (req, res) => {
     try {
         // change the name 
         await db.query('update users set name=? where user_id=?', [name, uid]);
+        req.session.userName = name;
         return res.status(200).json({ message: 'successfully updated' });
     } catch (error) {
         console.log(error);
